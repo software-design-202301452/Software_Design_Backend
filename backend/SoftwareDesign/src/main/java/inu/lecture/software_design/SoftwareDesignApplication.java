9573a64ea46d1213b9367952d8aa22d76d8332f0
d6nggyun <d6nggyun@gmail.com>
2026-03-26 01:16:25 +0900

    setting: rename package

--- backend/SoftwareDesign/src/main/java/inu/lecture/software_design/SoftwareDesignApplication.java
+++ backend/SoftwareDesign/src/main/java/inu/lecture/software_design/SoftwareDesignApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package inu.lecture.software_design;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/SoftwareDesign/src/test/java/inu/lecture/software_design/SoftwareDesignApplicationTests.java
+++ backend/SoftwareDesign/src/test/java/inu/lecture/software_design/SoftwareDesignApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package inu.lecture.software_design;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
