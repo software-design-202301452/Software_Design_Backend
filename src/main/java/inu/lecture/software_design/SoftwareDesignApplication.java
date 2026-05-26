31db69b98ea1a03bee3cc8d7f1dd0d3a96914bbe
d6nggyun <d6nggyun@gmail.com>
2026-05-26 18:42:39 +0900

    @EnableKafka 추가 및 SASL JAAS placeholder 기본값 설정

--- src/main/java/inu/lecture/software_design/SoftwareDesignApplication.java
+++ src/main/java/inu/lecture/software_design/SoftwareDesignApplication.java
@@ -3,12 +3,14 @@ package inu.lecture.software_design;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
 @EnableAsync
+@EnableKafka
 @SpringBootApplication
 public class SoftwareDesignApplication {
 
