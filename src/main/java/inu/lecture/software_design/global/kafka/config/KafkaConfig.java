9d6961669fbec1915e958eb3bb3b8a921f4b8597
d6nggyun <d6nggyun@gmail.com>
2026-05-26 18:25:11 +0900

    Kafka max.block.ms 30초로 증가 - 초기 연결 시간 확보

--- src/main/java/inu/lecture/software_design/global/kafka/config/KafkaConfig.java
+++ src/main/java/inu/lecture/software_design/global/kafka/config/KafkaConfig.java
@@ -40,7 +40,7 @@ public class KafkaConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 3000);
+        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 30000);
 
         if (!"PLAINTEXT".equals(securityProtocol) && !saslUsername.isBlank()) {
             props.put("security.protocol", securityProtocol);
