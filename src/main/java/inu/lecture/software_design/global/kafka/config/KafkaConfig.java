0c064f7cfc8f5ca3a666bb42023e97bf59ab6134
d6nggyun <d6nggyun@gmail.com>
2026-05-26 18:46:13 +0900

    kafkaListenerContainerFactory 명시적 빈 등록으로 Consumer 시작 오류 수정

--- src/main/java/inu/lecture/software_design/global/kafka/config/KafkaConfig.java
+++ src/main/java/inu/lecture/software_design/global/kafka/config/KafkaConfig.java
@@ -1,24 +1,29 @@
 package inu.lecture.software_design.global.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * CEW-114: Kafka 토픽 및 프로듀서 설정
- * Consumer는 Spring Boot auto-configuration에 위임 (application-prod.yml의 spring.kafka.properties 사용)
+ * CEW-114: Kafka 토픽, 프로듀서, 컨슈머 설정
  */
 @Configuration
 public class KafkaConfig {
@@ -35,14 +40,9 @@ public class KafkaConfig {
     @Value("${kafka.sasl.password:}")
     private String saslPassword;
 
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    private Map<String, Object> commonProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 30000);
-
+        props.put("bootstrap.servers", bootstrapServers);
         if (!"PLAINTEXT".equals(securityProtocol) && !saslUsername.isBlank()) {
             props.put("security.protocol", securityProtocol);
             props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256");
@@ -51,13 +51,40 @@ public class KafkaConfig {
                     "username=\"" + saslUsername + "\" " +
                     "password=\"" + saslPassword + "\";");
         }
+        return props;
+    }
 
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> props = commonProps();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 30000);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> props = commonProps();
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "analytics-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 
     @Bean
